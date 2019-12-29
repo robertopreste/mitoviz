b430305938aef9e84ab7e1be9592c28e11ce7e6a
Roberto Preste <roberto.preste@gmail.com>
2019-12-29 10:16:37 +0100

    TST: Add tests for plot_vcf function

--- tests/test_mitoviz.py
+++ tests/test_mitoviz.py
@@ -8,7 +8,7 @@ from click.testing import CliRunner
 import cv2
 import numpy as np
 
-from mitoviz import cli
+from mitoviz import cli, plot_vcf
 
 TESTDIR = os.path.dirname(os.path.realpath(__file__))
 DATADIR = os.path.join(TESTDIR, "data")
@@ -118,6 +118,85 @@ class TestCli(unittest.TestCase):
 
         # Then
         self.assertEqual(0, result.exit_code)
+        self.assertTrue(os.path.isfile(OUTPUT_MULTI_IMG))
+        diff = cv2.subtract(base_img, result_img)
+        self.assertFalse(np.any(diff))
+        # Cleanup
+        os.remove(OUTPUT_MULTI_IMG)
+
+
+class TestModule(unittest.TestCase):
+
+    def test_module_plot(self):
+        # Given
+        base_img = cv2.imread(BASE_IMG)
+
+        # When
+        plot_vcf(SAMPLE_VCF, save=True)
+        result_img = cv2.imread("MITOVIZ001.png")
+
+        # Then
+        self.assertTrue(os.path.isfile("MITOVIZ001.png"))
+        diff = cv2.subtract(base_img, result_img)
+        self.assertFalse(np.any(diff))
+        # Cleanup
+        os.remove("MITOVIZ001.png")
+
+    def test_module_plot_hf(self):
+        # Given
+        base_img = cv2.imread(BASE_HF_IMG)
+
+        # When
+        plot_vcf(SAMPLE_HF_VCF, save=True)
+        result_img = cv2.imread("HG00420.png")
+
+        # Then
+        self.assertTrue(os.path.isfile("HG00420.png"))
+        diff = cv2.subtract(base_img, result_img)
+        self.assertFalse(np.any(diff))
+        # Cleanup
+        os.remove("HG00420.png")
+
+    def test_module_plot_output(self):
+        # Given
+        base_img = cv2.imread(BASE_IMG)
+
+        # When
+        plot_vcf(SAMPLE_VCF, save=True, output=OUTPUT_IMG)
+        result_img = cv2.imread(OUTPUT_IMG)
+
+        # Then
+        self.assertTrue(os.path.isfile(OUTPUT_IMG))
+        diff = cv2.subtract(base_img, result_img)
+        self.assertFalse(np.any(diff))
+        # Cleanup
+        os.remove(OUTPUT_IMG)
+
+    def test_module_plot_output_hf(self):
+        # Given
+        base_img = cv2.imread(BASE_HF_IMG)
+
+        # When
+        plot_vcf(SAMPLE_HF_VCF, save=True, output=OUTPUT_HF_IMG)
+        result_img = cv2.imread(OUTPUT_HF_IMG)
+
+        # Then
+        self.assertTrue(os.path.isfile(OUTPUT_HF_IMG))
+        diff = cv2.subtract(base_img, result_img)
+        self.assertFalse(np.any(diff))
+        # Cleanup
+        os.remove(OUTPUT_HF_IMG)
+
+    def test_module_plot_sample_multi(self):
+        # Given
+        base_img = cv2.imread(BASE_MULTI_IMG)
+
+        # When
+        plot_vcf(SAMPLE_MULTI_VCF, sample="SRR1777294",
+                 save=True, output=OUTPUT_MULTI_IMG)
+        result_img = cv2.imread(OUTPUT_MULTI_IMG)
+
+        # Then
         self.assertTrue(os.path.isfile(OUTPUT_MULTI_IMG))
         diff = cv2.subtract(base_img, result_img)
         self.assertFalse(np.any(diff))
