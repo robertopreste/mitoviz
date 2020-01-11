21a85c1f2d169bcff56f1048a3e4eb541374f74a
Roberto Preste <roberto.preste@gmail.com>
2020-01-11 14:23:44 +0000

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(  # pragma: no cover
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/robertopreste/mitoviz",
-    version='0.2.2',
+    version='0.2.3',
     zip_safe=False,
 )
