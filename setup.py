d8cbbd3ec25594cfa42250d584050fee43439928
Roberto Preste <roberto.preste@gmail.com>
2020-02-14 18:48:21 +0000

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(  # pragma: no cover
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/robertopreste/mitoviz",
-    version='0.4.1',
+    version='0.4.2',
     zip_safe=False,
 )
