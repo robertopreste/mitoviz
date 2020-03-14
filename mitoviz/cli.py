1ad1ee37f3f5916d452e0bf4af0d73b7079ab585
Roberto Preste <roberto.preste@gmail.com>
2020-03-14 19:07:21 +0000

    ENH: Add option to create interactive polar plots

--- mitoviz/cli.py
+++ mitoviz/cli.py
@@ -23,34 +23,26 @@ from mitoviz.mitoviz import plot_table, plot_vcf
               help="Add legend to the plot.")
 @click.option("--split", "-p", default=False, is_flag=True,
               help="Plot split H and L strands.")
+@click.option("--interactive", "-i", default=False, is_flag=True,
+              help="Create an interactive version of the plot.")
 @click.option("--sep", "-S", default=",",
               help="Column delimiter used (if INPUT_FILE is not a VCF file)")
 @click.pass_context
-def main(ctx, input_file, linear, sample, output, labels, legend, split, sep):
+def main(ctx, input_file, linear, sample, output, labels, legend, split,
+         interactive, sep):
     """ Plot human mitochondrial variants available in INPUT_FILE. """
     ext = os.path.splitext(os.path.basename(input_file))[-1]
     if ext.casefold() == ".vcf":
-        plot_vcf(in_vcf=input_file,
-                 linear=linear,
-                 sample=sample,
-                 save=True,
-                 output=output,
-                 labels=labels,
-                 legend=legend,
-                 split=split)
+        plot_vcf(in_vcf=input_file, linear=linear, sample=sample, save=True,
+                 output=output, labels=labels, legend=legend, split=split,
+                 interactive=interactive)
     else:
         pandas_opts = dict()
         if ctx.args:
             pandas_opts.update([el.split("=") for el in ctx.args])
-        plot_table(in_table=input_file,
-                   sep=sep,
-                   linear=linear,
-                   sample=sample,
-                   save=True,
-                   output=output,
-                   labels=labels,
-                   legend=legend,
-                   split=split,
+        plot_table(in_table=input_file, sep=sep, linear=linear, sample=sample,
+                   save=True, output=output, labels=labels, legend=legend,
+                   split=split, interactive=interactive,
                    **pandas_opts)
 
     return 0
