6fe8c58cb65284861be5dea395b0a3633a7b986b
Roberto Preste <roberto.preste@gmail.com>
2020-04-05 17:05:54 +0100

    ENH: Add function to show base plots

--- mitoviz/mitoviz.py
+++ mitoviz/mitoviz.py
@@ -10,9 +10,53 @@ import pandas as pd
 from mitoviz.parsers import _DataFrameParser, _TabularParser, _VcfParser
 from mitoviz.plot import (
     _plot_variants_polar, _plot_variants_linear, _plotly_variants_polar,
-    _plotly_variants_linear
+    _plotly_variants_linear, _plot_mito_polar, _plot_mito_linear,
+    _plotly_mito_polar, _plotly_mito_linear
 )
 from mitoviz.utils import parse_path
+
+
+def plot_base(linear: bool = False,
+              save: bool = False,
+              output: Optional[str] = None,
+              legend: bool = False,
+              split: bool = False,
+              interactive: bool = False) -> None:
+    """ Plot the base mitochondrial representation.
+
+    Args:
+        linear: show a linear plot rather than a polar one [default: False]
+        save: if true, the final plot will be saved to a file [default: False]
+        output: path of the output file where the plot will be saved
+        legend: if true, add a legend for loci colors in the plot
+            [default: False]
+        split: if true, plot split H and L strands [default: True]
+        interactive: if true, create an interactive version of the plot
+            [default: False]
+    """
+    if linear:
+        if interactive:
+            plot_mito = _plotly_mito_linear
+        else:
+            plot_mito = _plot_mito_linear
+    else:
+        if interactive:
+            plot_mito = _plotly_mito_polar
+        else:
+            plot_mito = _plot_mito_polar
+
+    fig = plot_mito(legend, split)
+
+    if save:
+        dirname, name, ext = parse_path(output)
+        if name == "":
+            name = "base_mt"
+        if interactive:
+            fig.write_html(os.path.join(dirname, f"{name}.html"),
+                           auto_open=False)
+        else:
+            plt.savefig(os.path.join(dirname, f"{name}{ext}"))
+            plt.close()
 
 
 def plot_vcf(in_vcf: str,
