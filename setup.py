b8c41826800d57797aed57515c0cc4e6d719b91a
Roberto Preste <roberto.preste@gmail.com>
2020-04-20 19:10:55 +0100

    Bump version: 0.8.1 → 0.8.2

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(  # pragma: no cover
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/robertopreste/mitoviz",
-    version='0.8.1',
+    version='0.8.2',
     zip_safe=False,
 )
